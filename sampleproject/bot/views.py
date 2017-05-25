5c20418b8e5f6dc033d1a7c515d30d5e9b026db5
Francesco Scarlato (Jungmann) <francesco.scarlato@gmail.com>
2017-05-25 21:10:41 +0200

    Fix sampleproject view

--- sampleproject/bot/views.py
+++ sampleproject/bot/views.py
@@ -5,5 +5,5 @@ from django_telegrambot.apps import DjangoTelegramBot
 # Create your views here.
 def index(request):
     bot_list = DjangoTelegramBot.bots
-    context = {'bot_list': bot_list, 'update_mode':settings.TELEGRAM_BOT_MODE}
+    context = {'bot_list': bot_list, 'update_mode':settings.DJANGO_TELEGRAMBOT['MODE']}
     return render(request, 'bot/index.html', context)
