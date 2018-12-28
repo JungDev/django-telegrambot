903d4f721be7fa51aa0f6b2d63d77d43f1de2702
alistvt <alistvt@gmail.com>
2018-12-28 09:11:43 -0800

    Adding MessageQueue support to avoid hitting flood limits.

---
+++ django_telegrambot/mqbot.py
@@ -0,0 +1,30 @@
+from telegram.ext import messagequeue as mq
+import telegram
+
+class MQBot(telegram.bot.Bot):
+	'''A subclass of Bot which delegates send method handling to MQ'''
+	def __init__(self, mqueue=None, is_queued_def=True, *args, **kwargs):
+		super(MQBot, self).__init__(*args, **kwargs)
+		# below 2 attributes should be provided for decorator usage
+		self._is_messages_queued_default = is_queued_def
+		self._msg_queue = mqueue or mq.MessageQueue()
+
+	def __del__(self):
+		try:
+			self._msg_queue.stop()
+		except:
+			pass
+		super(MQBot, self).__del__()
+
+	@mq.queuedmessage
+	def send_message(self, *args, **kwargs):
+		'''Wrapped method would accept new `queued` and `isgroup`
+		OPTIONAL arguments'''
+		return super(MQBot, self).send_message(*args, **kwargs)
+
+	@mq.queuedmessage
+	def edit_message_text(self, *args, **kwargs):
+
+		'''Wrapped method would accept new `queued` and `isgroup`
+		OPTIONAL arguments'''
+		return super(MQBot, self).edit_message_text(*args, **kwargs)
