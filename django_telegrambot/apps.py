7f988e32bceae25c59774ba5741ec094110663e4
JungDev <jungmann.praga@gmail.com>
2019-01-02 16:26:48 +0100

    Update apps.py

--- django_telegrambot/apps.py
+++ django_telegrambot/apps.py
@@ -168,8 +168,8 @@ class DjangoTelegramBot(AppConfig):
 			allowed_updates = b.get('ALLOWED_UPDATES', None)
 			timeout = b.get('TIMEOUT', None)
             proxy = b.get('PROXY', None)
-
-		    if self.mode == WEBHOOK_MODE:
+            
+            if self.mode == WEBHOOK_MODE:
 				try:              
 					if b.get('MESSAGEQUEUE_ENABLED',False):
 						q = mq.MessageQueue(all_burst_limit=b.get('MESSAGEQUEUE_ALL_BURST_LIMIT',29),
